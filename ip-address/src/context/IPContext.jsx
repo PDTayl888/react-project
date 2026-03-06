887bf17855fa8f78640855f6e20901ec43f86bd2
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-05 19:23:58 -0500

    added mockData

---
+++ ip-address/src/context/IPContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState, useEffect, useContext } from 'react';
+
+
+const mockData = {
+    ip: "222.33.44.222",
+    isp: "Comcast",
+    location: {
+        city: "Pittsburgh",
+        region: "Pa",
+        postalCode: "15243",
+        timezone: "-05:000",
+        lat: 40.4406,
+        lng: -67.6767,
+    }
+};
+
